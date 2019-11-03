1de23f3b71a891be2234b60cb8b60b76c6473418
youxiang.yang <649315834@qq.com>
2019-11-03 15:53:55 +0800

    Inspect jcj point when tapping inspect button

--- app/src/main/java/com/njscky/mapcollect/MainActivity.java
+++ app/src/main/java/com/njscky/mapcollect/MainActivity.java
@@ -2,7 +2,6 @@ package com.njscky.mapcollect;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -17,7 +16,6 @@ import com.esri.android.map.Layer;
 import com.esri.android.map.MapView;
 import com.esri.android.map.event.OnSingleTapListener;
 import com.esri.core.geometry.Envelope;
-import com.esri.core.geometry.Geometry;
 import com.esri.core.geometry.Point;
 import com.esri.core.map.Graphic;
 import com.esri.core.tasks.identify.IdentifyParameters;
@@ -35,7 +33,6 @@ import com.njscky.mapcollect.util.PermissionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -50,6 +47,11 @@ import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_
 import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int STATE_INIT = 0;
+    private static final int STATE_PROJECT = 1;
+    private static final int STATE_INSPECT = 2;
+    private static final int STATE_QUERY = 3;
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -69,9 +71,17 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.btnGCGL)
     Button btnProject;
+    @BindView(R.id.btnInspect)
+    Button btnInspect;
+    @BindView(R.id.btnLayer)
+    Button btnLayer;
+    @BindView(R.id.btnQuery)
+    Button btnQuery;
 
     @BindView(R.id.bottom_menu)
     View bottomMenuLayout;
+
+    private int state;
 
     private Snackbar snackbar;
 
@@ -96,32 +106,18 @@ public class MainActivity extends AppCompatActivity {
         mMapView.setOnSingleTapListener(new OnSingleTapListener() {
             @Override
             public void onSingleTap(float x, float y) {
-                if (bottomSheetBehavior != null) {
-                    bottomSheetBehavior.setHideable(true);
-                    bottomSheetBehavior.setState(STATE_HIDDEN);
+                switch (state) {
+                    case STATE_INSPECT:
+                        singleTapInspect(x, y);
+                        break;
+                    case STATE_QUERY:
+                        singleTapQuery(x, y);
+                        break;
+                    case STATE_INIT:
+                    default:
+                        Log.i(TAG, "onSingleTap: " + state);
+                        break;
                 }
-
-//                Point p = mMapView.toMapPoint(x, y);
-//                mMapView.centerAt(p, true);
-
-                GraphicsLayer layer = (GraphicsLayer) layerManager.getYSPointLayer();
-
-                int[] graphicIds = layer.getGraphicIDs(x, y, 10);
-
-                if (graphicIds == null || graphicIds.length == 0) {
-                    Toast.makeText(MainActivity.this, "此处无检查井", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                List<Graphic> graphics = new ArrayList<>(graphicIds.length);
-
-                for (int graphicId : graphicIds) {
-                    Graphic graphic = layer.getGraphic(graphicId);
-                    Log.i(TAG, "initMapView: " + graphic.getGeometry());
-                    graphics.add(graphic);
-                }
-
-                choosePoints(layer, graphics);
             }
         });
 
@@ -191,6 +187,53 @@ public class MainActivity extends AppCompatActivity {
         mMapView.addLayer(tempGraphicsLayer);
     }
 
+    private void singleTapQuery(float x, float y) {
+        if (!mMapView.isLoaded()) {
+            return;
+        }
+        // establish the identify parameters
+        Point identifyPoint = mMapView.toMapPoint(x, y);
+        params.setGeometry(identifyPoint);// 设置识别位置
+        params.setSpatialReference(mMapView.getSpatialReference());// 设置坐标系
+        params.setMapHeight(mMapView.getHeight());// 设置地图像素高
+        params.setMapWidth(mMapView.getWidth());// 设置地图像素宽
+        Envelope env = new Envelope();
+        mMapView.getExtent().queryEnvelope(env);
+        params.setMapExtent(env);// 设置当前地图范围
+
+        MyIdentifyTask mTask = new MyIdentifyTask(tempGraphicsLayer);
+        mTask.execute(params);
+    }
+
+    private void singleTapInspect(float x, float y) {
+        if (bottomSheetBehavior != null) {
+            bottomSheetBehavior.setHideable(true);
+            bottomSheetBehavior.setState(STATE_HIDDEN);
+        }
+
+//                Point p = mMapView.toMapPoint(x, y);
+//                mMapView.centerAt(p, true);
+
+        GraphicsLayer layer = (GraphicsLayer) layerManager.getYSPointLayer();
+
+        int[] graphicIds = layer.getGraphicIDs(x, y, 10);
+
+        if (graphicIds == null || graphicIds.length == 0) {
+            Toast.makeText(MainActivity.this, "此处无检查井", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<Graphic> graphics = new ArrayList<>(graphicIds.length);
+
+        for (int graphicId : graphicIds) {
+            Graphic graphic = layer.getGraphic(graphicId);
+            Log.i(TAG, "initMapView: " + graphic.getGeometry());
+            graphics.add(graphic);
+        }
+
+        choosePoints(layer, graphics);
+    }
+
     private void choosePoints(GraphicsLayer layer, List<Graphic> graphics) {
         choosePointsDialog = new AlertDialog.Builder(this)
                 .setTitle(R.string.choose_points)
@@ -198,7 +241,6 @@ public class MainActivity extends AppCompatActivity {
                         new GraphicListAdpater(graphics),
                         (dialog, which) -> {
                             Graphic graphic = graphics.get(which);
-                            mMapView.setExtent(graphic.getGeometry());
                             Log.i(TAG, "choosePoints: " + graphic.getAttributeValue("JCJBH"));
                             JcjInspectFragment fragment = JcjInspectFragment.newInstance(graphic);
                             getSupportFragmentManager().beginTransaction()
@@ -265,50 +307,35 @@ public class MainActivity extends AppCompatActivity {
         ProjectActivity.startForResult(this, REQ_PROJECT);
     }
 
-    @OnClick(R.id.btnSJBJ)
-    void onEdit() {
-        // TODO 编辑
+    @OnClick(R.id.btnInspect)
+    void onInspect() {
+        if (state == STATE_INSPECT) {
+            state = STATE_INIT;
+        } else {
+            state = STATE_INSPECT;
+        }
+        updateState();
     }
 
-    @OnClick(R.id.btnTCKZ)
+    @OnClick(R.id.btnLayer)
     void onLayer() {
         // TODO 图层
     }
 
-    @OnClick(R.id.btnXJ)
-    void onPhoto() {
+    @OnClick(R.id.btnQuery)
+    void onQuery() {
         // TODO 背景管线查询
-        mMapView.setExtent(layerManager.getGxExtent());
-        mMapView.setOnSingleTapListener(null);
-        mMapView.setOnLongPressListener(null);
-
+        if (state == STATE_QUERY) {
+            state = STATE_INIT;
+        } else {
+            state = STATE_QUERY;
+        }
+        updateState();
         params = new IdentifyParameters();
         params.setTolerance(20); //设置容差
         params.setDPI(98);
         params.setLayers(new int[]{2, 3, 5, 6, 8, 9});
         params.setLayerMode(IdentifyParameters.TOP_MOST_LAYER);
-
-        mMapView.setOnSingleTapListener(new OnSingleTapListener() {
-            private static final long serialVersionUID = 1L;
-
-            public void onSingleTap(final float x, final float y) {
-                if (!mMapView.isLoaded()) {
-                    return;
-                }
-                // establish the identify parameters
-                Point identifyPoint = mMapView.toMapPoint(x, y);
-                params.setGeometry(identifyPoint);// 设置识别位置
-                params.setSpatialReference(mMapView.getSpatialReference());// 设置坐标系
-                params.setMapHeight(mMapView.getHeight());// 设置地图像素高
-                params.setMapWidth(mMapView.getWidth());// 设置地图像素宽
-                Envelope env = new Envelope();
-                mMapView.getExtent().queryEnvelope(env);
-                params.setMapExtent(env);// 设置当前地图范围
-
-                MyIdentifyTask mTask = new MyIdentifyTask(tempGraphicsLayer);
-                mTask.execute(params);
-            }
-        });
     }
 
     @Override
@@ -343,7 +370,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 Log.i(TAG, "loadData#onFinish: ");
-                mMapView.setExtent(layerManager.getPointExtent());
             }
         });
 
@@ -383,16 +409,38 @@ public class MainActivity extends AppCompatActivity {
         DbManager.getInstance(this).close();
     }
 
+    private void updateState() {
+        switch (state) {
+            case STATE_INSPECT:
+                btnProject.setEnabled(false);
+                btnInspect.setEnabled(true);
+                btnLayer.setEnabled(false);
+                btnQuery.setEnabled(false);
+                break;
+            case STATE_QUERY:
+                btnProject.setEnabled(false);
+                btnInspect.setEnabled(false);
+                btnLayer.setEnabled(false);
+                btnQuery.setEnabled(true);
+                break;
+            case STATE_INIT:
+            default:
+                btnProject.setEnabled(true);
+                btnInspect.setEnabled(true);
+                btnLayer.setEnabled(true);
+                btnQuery.setEnabled(true);
+                break;
+        }
+
+    }
+
     /*
      * 异步执行查询任务
      */
     private class MyIdentifyTask extends
             AsyncTask<IdentifyParameters, Void, IdentifyResult[]> {
         IdentifyTask mIdentifyTask;
-        private Graphic[] highlightGraphics;
-        private Context mContext;
         private GraphicsLayer mGraphicsLayer;
-        private Map<String, Object> map;
 
         public MyIdentifyTask(GraphicsLayer graphicsLayer) {
             this.mGraphicsLayer = graphicsLayer;
@@ -406,7 +454,6 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     mResult = mIdentifyTask.execute(mParams);// 执行识别任务
                 } catch (Exception e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
 
@@ -416,35 +463,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(IdentifyResult[] results) {
-            // TODO 显示查询结果
             if (results != null && results.length > 0) {
-                IdentifyResult tempResult = results[0];
-                String layerName = tempResult.getLayerName();
-                highlightGraphics = new Graphic[results.length];
-                Geometry geom = results[0].getGeometry();
-                String typeName = geom.getType().name();
-                map = results[0].getAttributes();
-                //显示到查询结果页面
-
                 QueryResultActivity.start(MainActivity.this, results);
-
-
-//                int color = Color.rgb(255, 255, 0);
-//                // 绘制点线面的要素
-//                if (typeName.equalsIgnoreCase("point")) {
-//                    SimpleMarkerSymbol sms = new SimpleMarkerSymbol(color, 20,
-//                            SimpleMarkerSymbol.STYLE.CIRCLE);
-//                    highlightGraphics[0] = new Graphic(geom, sms);
-//                } else if (typeName.equalsIgnoreCase("polyline")) {
-//                    SimpleLineSymbol sls = new SimpleLineSymbol(color, 5);
-//                    highlightGraphics[0] = new Graphic(geom, sls);
-//                }
-//                mGraphicsLayer.addGraphic(highlightGraphics[0]);
             } else {
-                Toast toast = Toast.makeText(mContext, "没有拾取到对象！",
-                        Toast.LENGTH_SHORT
-                );
-                toast.show();
+                Toast.makeText(MainActivity.this, "没有拾取到对象！", Toast.LENGTH_SHORT).show();
             }
         }
 
